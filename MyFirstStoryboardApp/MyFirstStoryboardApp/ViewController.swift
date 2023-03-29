776b89b52632a0c363f0412a196efa44792fe038
chynmn <chynmn0523@gmail.com>
2023-03-30 01:32:16 +0900

    Create MyFirestStoryboardApp project

---
+++ MyFirstStoryboardApp/MyFirstStoryboardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstStoryboardApp
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 2023/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
