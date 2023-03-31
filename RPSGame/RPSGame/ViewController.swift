b8b6c3ab7f78c78bf970c13c4dfebe1e52b445b9
chynmn <chynmn0523@gmail.com>
2023-03-31 22:28:42 +0900

    Create RPSGame Project

---
+++ RPSGame/RPSGame/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  RPSGame
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 2023/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // 변수 / 속성
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    
+    // 함수 / 메서드
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func rpsButtomTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+    
+    
+
+}
+
