70e16e247f5dc802f159dae1523d6ae087356b39
chynmn <chynmn0523@gmail.com>
2023-03-31 11:46:40 +0900

    [feat] Add logic in Label, Button

--- MyFirstStoryboardApp/MyFirstStoryboardApp/ViewController.swift
+++ MyFirstStoryboardApp/MyFirstStoryboardApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var myButton: UIButton!
+    
+    
+    // 앱의 화면에 들어오면 처음 실행시키는 함수
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        mainLabel.text = "방가방가"
+//        mainLabel.backgroundColor = UIColor.yellow
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        mainLabel.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        mainLabel.text = "안녕하세요"
+//        mainLabel.backgroundColor = UIColor.yellow
+        
+        myButton.backgroundColor = UIColor.yellow
+        myButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
