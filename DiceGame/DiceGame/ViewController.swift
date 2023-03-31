4e74cc3cf28a5456fcd6516be261b6ca17f5a0a4
chynmn <chynmn0523@gmail.com>
2023-03-31 21:55:46 +0900

    [feat] random dice logic

--- DiceGame/DiceGame/ViewController.swift
+++ DiceGame/DiceGame/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        firstImageView.image = diceArray[2]
+        secondImageView.image = diceArray[2]
+        
     }
 
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        
+        // 첫 번째 이미지 뷰의 이미지를 랜덤으로 변경
+        firstImageView.image = diceArray.randomElement()
+        
+        // 두 번째 이미지 뷰의 이미지를 랜덤으로 변경
+        secondImageView.image = diceArray.randomElement()
+        
+    }
+    
+    
 
 }
 
