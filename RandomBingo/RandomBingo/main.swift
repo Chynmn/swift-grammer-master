913f09e9edb1b6cc7b07ade204e418e52e497827
chynmn <chynmn0523@gmail.com>
2023-04-04 19:34:58 +0900

    add logic

--- RandomBingo/RandomBingo/main.swift
+++ RandomBingo/RandomBingo/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+var computerChoice = Int.random(in: 1...100)
+var myChoice = 0
 
+while true {
+    
+    var userInput = readLine()
+    
+    if let input = userInput {
+        if let number = Int(input) {
+            myChoice = number
+        }
+    }
+    
+    // print(myChoice)
+    
+    if computerChoice > myChoice {
+        print("Up")
+    } else if computerChoice < myChoice {
+        print("Down")
+    } else {
+        print("Bingo")
+        break   // 반복문 종료
+    }
+    
+    
+}
