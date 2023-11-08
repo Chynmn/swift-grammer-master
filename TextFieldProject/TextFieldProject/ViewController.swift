e2675e2e390807cd3b9ac01184208430ad880e20
chynmn <chynmn0523@gmail.com>
2023-11-08 16:52:22 +0900

    Create TextField for Delegate Pattern

---
+++ TextFieldProject/TextFieldProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TextFieldProject
+//
+//  Created by /Chynmn/M1 pro—̳͟͞͞♡ on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.gray
+    }
+
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+
+}
+
