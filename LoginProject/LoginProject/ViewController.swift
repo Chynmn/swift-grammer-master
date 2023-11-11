68e7befbc7a0a320d824e53349f58bc81c2ef491
chynmn <chynmn0523@gmail.com>
2023-11-11 19:08:07 +0900

    Auto layout by code

--- LoginProject/LoginProject/ViewController.swift
+++ LoginProject/LoginProject/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 메모리상에만 존재할 뿐 실제 뷰에는 보여지지 않는 상태
+    let emailTextFieldView = UIView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        makeUI()
     }
 
+    func makeUI() {
+        emailTextFieldView.backgroundColor = UIColor.darkGray
+        
+        // 뷰 하위에 올리는 코드
+        view.addSubview(emailTextFieldView)
+        
+        // 자동 오토 레이아웃 설정을 끄고 수동으로 설정.
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        // 왼쪽 오토 레이아웃
+        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        // 오른쪽 오토 레이아웃
+        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        // 위쪽 오토 레이아웃
+        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        // 높이 설정
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    }
+    
+    
 
 }
 
